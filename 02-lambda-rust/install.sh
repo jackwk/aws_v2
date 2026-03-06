01785bea62fcb0709f97f6fe9c22ec0f01774d9c
Jacek Walerczyk <jacek.walerczyk@contractors.roche.com>
2026-03-06 10:31:01 +0100

    updated install

--- 02-lambda-rust/install.sh
+++ 02-lambda-rust/install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Redirect all output to a custom log file
+exec > /var/log/user-data-setup.log 2>&1
+# Print each command before executing it (great for debugging)
+set -x 
+
 # Update the system
 sudo dnf update -y
 
@@ -10,18 +15,15 @@ wget https://ziglang.org/download/0.13.0/zig-linux-aarch64-0.13.0.tar.xz
 tar -xf zig-linux-aarch64-0.13.0.tar.xz
 sudo mv zig-linux-aarch64-0.13.0 /usr/local/zig
 
-# Update PATH for Zig
-export PATH="/usr/local/zig:$PATH"
-echo 'export PATH="/usr/local/zig:$PATH"' >> ~/.bashrc
-zig version
+# Update PATH for Zig globally so all users (including ec2-user) can use it
+echo 'export PATH="/usr/local/zig:$PATH"' | sudo tee /etc/profile.d/zig.sh
 
-# Install Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# Install Rust specifically for the ec2-user
+sudo -u ec2-user curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sudo -u ec2-user sh -s -- -y
 
-# Install Cargo Lambda
-curl -fsSL https://cargo-lambda.info/install.sh | sh
-echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
+# Install Cargo Lambda specifically for the ec2-user
+# We wrap this in a bash command so we can source the newly installed Rust environment first
+sudo -u ec2-user bash -c 'source $HOME/.cargo/env && curl -fsSL https://cargo-lambda.info/install.sh | sh'
 
 # Install Terraform
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
